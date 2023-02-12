56c9551f90a964fe35b1111eeebf153950fffdc3
Oscar Fernández Muñoz <oscarfmdev@gmail.com>
2023-02-12 10:37:27 -0600

    Add server-side render with handlebars

--- app.js
+++ app.js
@@ -3,18 +3,29 @@ const app = express()
 
 const port = 8080
 
+app.set('view engine', 'hbs');
 
 // Serve static content
 app.use( express.static('public') );
 
 
-app.get('/hello', (req, res) => {
-  res.send('This is a test')
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.get('/generic', (req, res) => {
+  res.sendFile(__dirname + '/public/generic.html')
+})
+
+app.get('/elements', (req, res) => {
+  res.sendFile(__dirname + '/public/elements.html')
 })
 
 app.get('*', (req, res) => {
   res.sendFile( __dirname + '/public/404.html')
 })
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
