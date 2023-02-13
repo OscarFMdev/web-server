0d5d1428946cc9ef68f003bc042e2d8738193eec
Oscar Fernández Muñoz <oscarfmdev@gmail.com>
2023-02-13 09:12:40 -0600

    Add partials

--- app.js
+++ app.js
@@ -1,31 +1,45 @@
 const express = require('express')
-const app = express()
+const hbs = require('hbs');
 
+const app = express()
 const port = 8080
 
+
+// Handlebars
 app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials', function (err) {});
 
 // Serve static content
 app.use( express.static('public') );
 
 
 app.get('/', (req, res) => {
-  res.render('home')
-})
+  res.render('home', {
+    name: 'Oscar Fernández Muñoz',
+    title: 'Software developer'
+  });
+});
 
 app.get('/generic', (req, res) => {
-  res.sendFile(__dirname + '/public/generic.html')
-})
+  res.render('generic', {
+    name: 'Oscar Fernández Muñoz',
+    title: 'Software developer'
+  });
+});
 
 app.get('/elements', (req, res) => {
-  res.sendFile(__dirname + '/public/elements.html')
-})
+  res.render('elements', {
+    name: 'Oscar Fernández Muñoz',
+    title: 'Software developer'
+  });
+});
+
 
 app.get('*', (req, res) => {
-  res.sendFile( __dirname + '/public/404.html')
-})
+  res.sendFile( __dirname + '/public/back/404.html')
+});
 
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
