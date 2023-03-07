5bc31fcb06c5a84d258d4bbf8119bc11076dd0c1
Oscar Fernández Muñoz <oscarfmdev@gmail.com>
2023-03-06 20:42:45 -0600

    Webserver ready and ignore DS_Store

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-const express = require('express')
+require('dotenv').config();
+const express = require('express');
 const hbs = require('hbs');
 
-const app = express()
-const port = 8080
+const app = express();
+const port = process.env.PORT;
 
 
 // Handlebars
